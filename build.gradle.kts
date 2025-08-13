d16829ce46b85287c89b808bdec1006b15b7ab18
GordoxGit <romain.begot@gmail.com>
2025-08-13 19:51:48 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins { java }
+group = "com.example"
+version = "1.4.0"
+
+repositories {
+  maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+  mavenCentral()
+}
+
+dependencies {
+  compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+tasks.withType<JavaCompile> {
+  options.encoding = "UTF-8"
+  options.release.set(21)
+}
